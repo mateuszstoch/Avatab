91fe084d52563d310ecd2c47b71f4e4d56737e63
Mateusz Stoch <matis2k4@gmail.com>
2024-09-30 22:14:04 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Avatab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
