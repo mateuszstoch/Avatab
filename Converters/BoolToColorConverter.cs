8c34b76f68f8d46c4bb97b16ad4182047f9cbf78
Mateusz Stoch <matis2k4@gmail.com>
2025-05-17 23:37:11 +0200

    enchanced mainview

---
+++ Converters/BoolToColorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Avatab.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Colors.Red : Colors.LightGray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
