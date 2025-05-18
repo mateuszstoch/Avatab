1e604d36ffd7ccc6b3f4ec38742c6cc40e966d26
Mateusz Stoch <matis2k4@gmail.com>
2025-05-18 16:41:09 +0200

    modernized look for import popup

---
+++ Converters/StringNotEmptyConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Avatab.Converters
+{
+    public class StringNotEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            !string.IsNullOrWhiteSpace(value as string);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
