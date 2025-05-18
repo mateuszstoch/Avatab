3288fe19e0020671c4b37cec6e9d4da210669ce3
Mateusz Stoch <mateusz.stoch75@gmail.com>
2025-05-18 21:19:56 +0200

    added page to edit person

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace Avatab
+﻿using Avatab.View;
+
+namespace Avatab
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(Routes.PersonEditPage, typeof(PersonEditPage));
         }
     }
 }
--- Services/Interfaces/IDatabaseService.cs
+++ Services/Interfaces/IDatabaseService.cs
@@ -6,7 +6,8 @@ namespace Avatab.Services.Interfaces
     {
         public void AddPerson(DBPerson person);
         public void AddLecture(DBLecture lecture);
-        public List<DBLecture> GetLectures(int parentId, DateTime date);
+        public List<DBLecture> GetLectureOnTime(int parentId, DateTime date);
+        public List<DBLecture> GetLecturesOnDay(int parentId, DateTime date);
         public List<DBLecture> GetAllLectures(int parentId);
         public List<DBPerson> GetAllPeople();
         public void UpdatePerson(DBPerson person);
